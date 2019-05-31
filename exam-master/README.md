642552cd97c413d793892e7c37536f3f74ec56d9
Kaisla Vuorenmaa <kaisla.vuorenmaa@helsinki.fi>
2019-05-31 10:18:19 +0300

    koemateriaali

---
+++ exam-master/README.md
@@ -0,0 +1,5 @@
+# Tools for the exam 
+
+Please clone this project before you start the exam.
+
+Contents of unix-like-wiki.html are taken from the Unix-like [Wikipedia page](https://en.wikipedia.org/wiki/Unix-like), released under CC-BY-SA:  http://creativecommons.org/licenses/by-sa/3.0/.
---
+++ exam-master/run_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# a really simple encryption technique: https://en.wikipedia.org/wiki/Caesar_cipher
+# "echo" is a bash command, and tr is a command line tool: https://en.wikipedia.org/wiki/Tr_(Unix)
+echo 'mjd_stb_dtzwj_fs_fqq_xyfw_ljy_dtzw_lfrj_ts_lt_uqfd' | tr '[f-za-e]' '[a-z]'
